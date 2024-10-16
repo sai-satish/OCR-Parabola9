9597905426c214b2fe45fc65b6ba21103646425c
sai-satish <saisatish.nilla1@gmail.com>
2024-10-16 16:09:28 +0530

    Added dependecies import

---
+++ imports.bash
@@ -0,0 +1,25 @@
+!pip install opencv-python
+!pip install paddleocr
+!pip install paddlepaddle
+!pip install easyocr
+!pip install boto3
+!pip install --upgrade setuptools
+!pip install --no-cache-dir paddleocr
+!python -m pip install paddlepaddle-gpu==2.5.0.post118 -f
+!pip install paddlepaddle
+!pip install paddleocr
+!pip install --upgrade setuptools
+!pip install transformers
+!pip install --upgrade transformers
+!pip install pytesseract
+!pip install --upgrade jupyterlab ipython
+
+!pip install setuptools==65.5.0
+!pip install fire
+!pip install paddleocr
+!rm -rf /home/ec2-user/.paddleocr/whl/det/en/en_PP-OCRv3_det_infer/*
+
+!pip install fastapi uvicorn python-multipart opencv-python-headless
+!pip install pyngrok
+!ngrok authtoken <Your-Auth-Token>
+!pip install aiofiles
